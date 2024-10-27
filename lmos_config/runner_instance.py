e7a233282f9d50c8bfcbc91ab3a3de0f4bd2b869
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-10-27 12:44:01 +0000

    add runner instance config

---
+++ lmos_config/runner_instance.py
@@ -0,0 +1,13 @@
+"""This module provides a globally defined mechanism for a runner component
+to determine what model instance config it is supposed to load"""
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+class RunnerInstanceSettings(BaseSettings) :
+    type: str = Field(description="the name of the model to run")
+
+    class Config:
+        env_prefix = "LMOS_RUNNER_"
+
+InstanceConfig = RunnerInstanceSettings()
