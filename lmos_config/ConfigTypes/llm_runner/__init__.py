07e62dbe4dcb37dac98a734b107a703a69defc6e
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-11-05 22:32:41 +0000

    add aphrodite args to config

--- lmos_config/ConfigTypes/llm_runner/__init__.py
+++ lmos_config/ConfigTypes/llm_runner/__init__.py
@@ -4,8 +4,10 @@ from pydantic import Field
 from ..generic.service import ExternalService
 from .exllama import ExllamaV2Runner
 from .vllm import vLLMRunner
+from .aphrodite import AphroditeRunner
 
 
 LLM_RUNNERS = Annotated[
-    Union[ExllamaV2Runner, vLLMRunner, ExternalService], Field(discriminator="type")
+    Union[ExllamaV2Runner, vLLMRunner, AphroditeRunner, ExternalService],
+    Field(discriminator="type"),
 ]
